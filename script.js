8fe74ed41ff59f54a5fe7757334d19c39c6e4c15
Ivona Zelenska <ney5744@gmail.com>
2024-11-25 14:27:13 +0000

    Your commit messagehomework1

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Part 1 //
+
+let number1 = '8';
+let number2 = '3';
+
+console.log( number1 + number2);             
+console.log( number1 - number2);          
+console.log( number1 * number2);       
+console.log(number1 / number2);             
+console.log(number1**3);       
+console.log( Math.sqrt(number1));     
+
+
+// Part 2 //
+
+
+let number = '25'; 
+
+console.log(String(number));      // string
+console.log(Number(number));      // number
+console.log(Boolean(number));    // boolean
